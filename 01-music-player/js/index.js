947378265dd494c086e738348a783832780537d4
felipeog <17603069+felipeog@users.noreply.github.com>
2023-01-08 19:29:43 -0300

    Improve formatter

--- 01-music-player/js/index.js
+++ 01-music-player/js/index.js
@@ -134,6 +134,10 @@ async function setCurrentMusic(music) {
 }
 
 function formatSeconds(initialSeconds) {
+  if (isNaN(initialSeconds)) {
+    return "00:00";
+  }
+
   const minutes = String(Math.floor(initialSeconds / 60)).padStart(2, "0");
   const seconds = String(Math.floor(initialSeconds % 60)).padStart(2, "0");
 
