786786e544bf37c393fa6e87f4b906652807219b
felipeog <17603069+felipeog@users.noreply.github.com>
2023-01-09 20:17:50 -0300

    Remove todo

--- 01-music-player/js/index.js
+++ 01-music-player/js/index.js
@@ -1,6 +1,5 @@
 // TODO: break in multiple files?
 // TODO: add try/catch
-// TODO: check if it's a chrome browser
 
 // elements
 const chooseMusicButton = document.querySelector(".choose-music-button");
