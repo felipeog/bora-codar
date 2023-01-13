e6e9b5b517dfe712ad77a7fed30a6dd6a73cc43b
felipeog <17603069+felipeog@users.noreply.github.com>
2023-01-13 15:14:43 -0300

    Move code to `src`

--- 01-music-player/vite.config.js
+++ 01-music-player/vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
+  root: "./src",
+  plugins: [mkcert()],
   server: {
     https: true,
   },
-  plugins: [mkcert()],
 });
