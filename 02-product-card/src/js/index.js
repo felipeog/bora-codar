fbf4a22b334773a050f3be4e0f39fbcc49aaad2a
felipeog <17603069+felipeog@users.noreply.github.com>
2023-01-15 17:58:18 -0300

    Change experience; Add styles

--- 02-product-card/src/js/index.js
+++ 02-product-card/src/js/index.js
@@ -1,20 +1,9 @@
-import { elements } from "./objects/elements";
+// TODO: add responsiveness
+// TODO: optimize sprites file
 
-elements.range.addEventListener("input", (event) => {
-  elements.image.style.backgroundPositionY = `${event.target.value * 253}px`;
-});
+const image = document.querySelector(".rotate__image");
+const range = document.querySelector(".rotate__range");
 
-elements.activateButton.addEventListener("click", (event) => {
-  elements.activateButton.classList.add("hidden");
-  elements.deactivateButton.classList.remove("hidden");
-  elements.range.classList.remove("hidden");
-});
-
-elements.deactivateButton.addEventListener("click", (event) => {
-  elements.activateButton.classList.remove("hidden");
-  elements.deactivateButton.classList.add("hidden");
-  elements.range.classList.add("hidden");
-
-  elements.image.style.backgroundPositionY = 0;
-  elements.range.value = 0;
+range.addEventListener("input", (event) => {
+  image.style.backgroundPositionY = `${event.target.value * 253}px`;
 });
--- 02-product-card/src/js/objects/elements.js
+++
@@ -1,6 +0,0 @@
-export const elements = {
-  activateButton: document.querySelector(".activate-button"),
-  deactivateButton: document.querySelector(".deactivate-button"),
-  image: document.querySelector(".rotate-image"),
-  range: document.querySelector(".rotate-range"),
-};
