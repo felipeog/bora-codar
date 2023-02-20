aec9d8d9a98c4bd4e6d177039f38d7a71bb1cd4d
felipeog <17603069+felipeog@users.noreply.github.com>
2023-02-20 19:51:38 -0300

    Style toggle

--- 07-carnival/src/components/Content.jsx
+++ 07-carnival/src/components/Content.jsx
@@ -16,19 +16,37 @@ import { MapView } from "./MapView";
 function Content() {
   return (
     <Flex width={960} marginX="auto" paddingY={20} as="section">
-      <Tabs variant="soft-rounded" width="100%">
+      <Tabs variant="unstyled" width="100%">
         <HStack justifyContent="space-between">
           <Heading as="h1" size="md">
             Blocos recomendados
           </Heading>
 
-          <TabList>
-            <Tab>
+          <TabList
+            border="1px solid var(--chakra-colors-gray-200)"
+            borderRadius={4}
+            padding={1}
+          >
+            <Tab
+              borderRadius={4}
+              paddingX={3}
+              paddingY={1}
+              fontWeight="bold"
+              color="blue.500"
+              _selected={{ backgroundColor: "blue.500", color: "white" }}
+            >
               <Text textTransform="uppercase" fontSize="xs">
                 Lista
               </Text>
             </Tab>
-            <Tab>
+            <Tab
+              borderRadius={4}
+              paddingX={3}
+              paddingY={1}
+              fontWeight="bold"
+              color="blue.500"
+              _selected={{ backgroundColor: "blue.500", color: "white" }}
+            >
               <Text textTransform="uppercase" fontSize="xs">
                 Mapa
               </Text>
