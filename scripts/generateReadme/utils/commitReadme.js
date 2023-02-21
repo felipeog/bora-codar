8dcc3b6e106386a9f6d38522f654099408ac3670
felipeog <17603069+felipeog@users.noreply.github.com>
2023-02-21 11:30:29 -0300

    Update script

--- scripts/generateReadme/utils/commitReadme.js
+++ scripts/generateReadme/utils/commitReadme.js
@@ -5,18 +5,17 @@ import { user } from "../consts/user.js";
 export function commitReadme() {
   console.log("Commiting...");
 
+  quietExecSync(`git config user.email "${user.email}"`);
+  quietExecSync(`git config user.name "${user.name}"`);
+  quietExecSync("git add readme.md");
+
   try {
-    quietExecSync(`git config user.email "${user.email}"`);
-    quietExecSync(`git config user.name "${user.name}"`);
-    quietExecSync("git add readme.md");
-    quietExecSync(
-      'git diff-index --quiet HEAD || git commit -m "Update readme.md"'
-    );
+    quietExecSync('git commit -m "Update readme.md"');
     quietExecSync("git push -u origin HEAD");
 
     console.log("Done");
   } catch (error) {
-    console.log(`Error commiting: ${error}`);
+    console.log("No changes to commit");
   }
 }
 
