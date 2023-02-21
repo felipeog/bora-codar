31df11719e9171d08b4f70dc69c5524638b057e5
felipeog <17603069+felipeog@users.noreply.github.com>
2023-02-20 21:42:16 -0300

    Improve search

--- 07-carnival/src/stores/filterStore.js
+++ 07-carnival/src/stores/filterStore.js
@@ -15,18 +15,39 @@ function applyFilters(state) {
   let filteredBlocks = blocks;
 
   if (state.title) {
-    filteredBlocks = filteredBlocks.filter((block) =>
-      block.title.includes(state.title)
-    );
+    filteredBlocks = filterBlocks({
+      key: "title",
+      search: state.title,
+      blocks: filteredBlocks,
+    });
   }
 
   if (state.location) {
-    filteredBlocks = filteredBlocks.filter(
-      (block) => block.location === state.location
-    );
+    filteredBlocks = filterBlocks({
+      key: "location",
+      search: state.location,
+      blocks: filteredBlocks,
+    });
   }
 
   return { filteredBlocks };
+}
+
+function filterBlocks({ key, search, blocks }) {
+  const normalizedSearch = normalizeString(search);
+
+  return blocks.filter((block) => {
+    const normalizedValue = normalizeString(block[key]);
+
+    return normalizedValue.includes(normalizedSearch);
+  });
+}
+
+function normalizeString(string) {
+  return string
+    .normalize("NFD")
+    .toLowerCase()
+    .replace(/[\u0300-\u036f]/g, "");
 }
 
 export { useFilterStore };
